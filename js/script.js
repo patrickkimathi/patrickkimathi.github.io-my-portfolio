e5b90e26b2949b6b8ca140d9413c97cb0098e58f
Patrick Kariuki <48854530+patrickkimathi@users.noreply.github.com>
2025-09-30 16:56:17 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-// ===== Mobile Menu Toggle =====
-const menuToggle = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
 
+// ===== Mobile Menu Toggle =====
+ const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
-  menuToggle.classList.toggle("open");
 });
 
 
